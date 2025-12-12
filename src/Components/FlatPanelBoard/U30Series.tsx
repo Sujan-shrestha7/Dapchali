81faf2daaac2ebd0ec895e579d68e4041323cdf3
Sujan-shrestha7 <sujanstha5732@gmail.com>
2025-12-12 12:29:09 +0545

    sf

--- src/Components/FlatPanelBoard/U30Series.tsx
+++ src/Components/FlatPanelBoard/U30Series.tsx
@@ -6,7 +6,7 @@ import u30Image from "../../assets/images/U3.png";
 const U30Series: React.FC = () => {
   return (
     <div>
-        <Head/>
+      <Head />
       <div className="px-[20px] md:px-[100px] py-[40px] md:py-[80px] flex flex-col gap-y-[80px] overflow-hidden">
         {/* Events */}
         <motion.div
@@ -17,13 +17,15 @@ const U30Series: React.FC = () => {
           viewport={{ once: true, amount: 0.2 }}
         >
           <div className="flex-1 flex flex-col gap-4">
-            <h2 className="font-semibold text-xl md:text-2xl text-center text-[#10507a]">
-              EVENTS / NOTICE
+            <h2 className="font-semibold text-xl md:text-2xl text-[#10507a]">
+              The Google Ecosystem
             </h2>
             <p className="text-gray-700 text-justify text-sm md:text-base leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-              voluptate culpa atque natus officia illum, nihil, assumenda autem
-              excepturi hic dolores officiis cumque, ad quaerat!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam With
+              the ELDA- certifies U3 series, you can access Google's expansive
+              suite of apps freely and securely. It's built in collabration
+              tools and intuitive operating system ensures a smooth and
+              hassle-free start.
             </p>
           </div>
           <img
